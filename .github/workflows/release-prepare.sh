8ce30a1787243632304bd3d184822d1b8ebe17b1
Sebastian Pekarek <mail@sebbo.net>
2020-04-24 23:27:58 +0200

    feat: Add Sentry integration

--- .github/workflows/release-prepare.sh
+++ .github/workflows/release-prepare.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-# sentry-cli releases new -p ble2mqtt $VERSION
-# sentry-cli releases set-commits $VERSION --auto
+sentry-cli releases new -p ble2mqtt $VERSION
+sentry-cli releases set-commits $VERSION --auto
--- .github/workflows/release-success.sh
+++ .github/workflows/release-success.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-# sentry-cli releases finalize ${VERSION}
+sentry-cli releases finalize ${VERSION}
